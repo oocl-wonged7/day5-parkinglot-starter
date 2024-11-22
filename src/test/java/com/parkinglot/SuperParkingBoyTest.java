09d8ab7fe6ca22a5b0890a267b402273bdb76e59
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-22 23:24:21 +0800

    [Story 6] test: add test case for park to parking log that being added first when multiple parking lot with same max available position rate

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -20,4 +20,21 @@ public class SuperParkingBoyTest {
         // Then
         assert (secondParkingLot.getName().equals(ticket.getIssuedBy()));
     }
+
+    @Test
+    void should_park_to_parking_lot_added_first_when_park_given_2_parking_lot_with_same_available_position_rate() {
+        // Given
+        ParkingLot firstParkingLot = new ParkingLot("First Parking Lot", 20);
+        ParkingLot secondParkingLot = new ParkingLot("Second Parking Lot", 20);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy();
+        smartParkingBoy.addParkingLot(firstParkingLot);
+        smartParkingBoy.addParkingLot(secondParkingLot);
+        Car car = new Car();
+
+        // When
+        Ticket ticket = smartParkingBoy.park(car);
+
+        // Then
+        assert (firstParkingLot.getName().equals(ticket.getIssuedBy()));
+    }
 }
