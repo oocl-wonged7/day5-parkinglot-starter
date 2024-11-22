e207400ad21c10b1024441ca1c93dfce036a3589
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-22 23:26:36 +0800

    refactor: replace for loop with stream

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -14,11 +14,10 @@ public class StandardParkingBoy extends ParkingBoy {
     public Ticket park(Car car) {
         List<ParkingLot> parkingLotList = new ArrayList<>(parkingLots.values());
         Collections.reverse(parkingLotList);
-        for (ParkingLot parkingLot : parkingLotList) {
-            if (parkingLot.getCurrentCapacity() < parkingLot.getCapacity()) {
-                return parkingLot.park(car);
-            }
-        }
-        throw new NoAvailablePositionException();
+        return parkingLotList.stream()
+                .filter(parkingLot -> parkingLot.getCurrentCapacity() < parkingLot.getCapacity())
+                .findFirst()
+                .orElseThrow(NoAvailablePositionException::new)
+                .park(car);
     }
 }
