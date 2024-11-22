afc557a9056c73c915d6e6e1202aff1915bb04e2
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-22 15:24:17 +0800

    [Story 3] test: add fetch twice with two tickets test for parking boy

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -33,4 +33,23 @@ public class ParkingBoyTest {
         // Then
         assert(carFetched.equals(car));
     }
+
+    @Test
+    void should_return_right_car_when_fetch_twice_given_two_ticket_and_a_parking_lot() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot("Good Parking Lot");
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car firstCar = new Car();
+        Ticket firstTicket = parkingBoy.park(firstCar);
+        Car secondCar = new Car();
+        Ticket secondTicket = parkingBoy.park(secondCar);
+
+        // When
+        Car carFatchedFirst = parkingBoy.fetch(firstTicket);
+        Car carFatchedSecond = parkingBoy.fetch(secondTicket);
+
+        // Then
+        assert (carFatchedFirst.equals(firstCar));
+        assert (carFatchedSecond.equals(secondCar));
+    }
 }
