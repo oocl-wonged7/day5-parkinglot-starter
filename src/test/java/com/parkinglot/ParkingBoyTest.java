ce620cec85413961e6e5940ad09353d9aaf51bf3
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-22 15:20:09 +0800

    [Story 3] feat: add parking boy to park car

---
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -0,0 +1,21 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingBoyTest {
+    @Test
+    void should_return_ticket_when_park_given_a_car_and_a_parking_lot() {
+        // Given
+        ParkingLot parkingLot = new ParkingLot("Good Parking Lot");
+        ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+        Car car = new Car();
+
+        // When
+        Ticket ticket = parkingBoy.park(car);
+
+        // Then
+        assertNotNull(ticket);
+    }
+}
